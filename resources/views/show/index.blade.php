dec3e843a900253f4fc1afc6f8de31304f0036dc
kuhblume <kasumigaura0524@yahoo.co.jp>
2018-01-31 18:12:59 +0900

    add seeder

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>project0</title>
+    <title>Play OTOGE!!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="サイト説明">
     <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
